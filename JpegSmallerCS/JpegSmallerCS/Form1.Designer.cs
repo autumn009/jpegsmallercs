0d99df0d933491ab8bbb06706ece9fc066803e39
Akira Kawamata <autumn@piedey.co.jp>
2021-06-04 16:01:57 +0900

    support settings

--- JpegSmallerCS/JpegSmallerCS/Form1.Designer.cs
+++ JpegSmallerCS/JpegSmallerCS/Form1.Designer.cs
@@ -37,6 +37,9 @@ namespace JpegSmallerCS
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSmallerTo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -54,18 +57,19 @@ namespace JpegSmallerCS
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxInputFiles.Location = new System.Drawing.Point(12, 18);
             this.textBoxInputFiles.Name = "textBoxInputFiles";
-            this.textBoxInputFiles.Size = new System.Drawing.Size(308, 23);
+            this.textBoxInputFiles.Size = new System.Drawing.Size(373, 23);
             this.textBoxInputFiles.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(337, 17);
+            this.button1.Location = new System.Drawing.Point(391, 17);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(21, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
@@ -82,22 +86,22 @@ namespace JpegSmallerCS
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxOutpurDir.Location = new System.Drawing.Point(12, 62);
             this.textBoxOutpurDir.Name = "textBoxOutpurDir";
-            this.textBoxOutpurDir.Size = new System.Drawing.Size(308, 23);
+            this.textBoxOutpurDir.Size = new System.Drawing.Size(373, 23);
             this.textBoxOutpurDir.TabIndex = 4;
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(337, 62);
+            this.button2.Location = new System.Drawing.Point(391, 62);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(21, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "...";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 102);
+            this.button3.Location = new System.Drawing.Point(12, 147);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
@@ -107,7 +111,7 @@ namespace JpegSmallerCS
             // button4
             // 
             this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button4.Location = new System.Drawing.Point(337, 102);
+            this.button4.Location = new System.Drawing.Point(337, 147);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 7;
@@ -115,11 +119,41 @@ namespace JpegSmallerCS
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "&Smaller to (pixel)";
+            // 
+            // textBoxSmallerTo
+            // 
+            this.textBoxSmallerTo.Location = new System.Drawing.Point(12, 106);
+            this.textBoxSmallerTo.Name = "textBoxSmallerTo";
+            this.textBoxSmallerTo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxSmallerTo.TabIndex = 9;
+            this.textBoxSmallerTo.Text = "400";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(105, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(215, 40);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Shrink JPEG File";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 134);
+            this.ClientSize = new System.Drawing.Size(414, 182);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSmallerTo);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -130,6 +164,8 @@ namespace JpegSmallerCS
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "JpegSmallerCS";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,6 +181,9 @@ namespace JpegSmallerCS
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSmallerTo;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- JpegSmallerCS/JpegSmallerCS/Form1.cs
+++ JpegSmallerCS/JpegSmallerCS/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace JpegSmallerCS
 {
@@ -20,6 +21,39 @@ namespace JpegSmallerCS
         private void button4_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Path.GetDirectoryName(textBoxInputFiles.Text);
+                openFileDialog.Filter = "JPEG files (*.jpg)|*.jpg|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    textBoxInputFiles.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            JpegSmallerCS.Properties.Settings.Default.InputFileName = textBoxInputFiles.Text;
+            JpegSmallerCS.Properties.Settings.Default.OutputDir = textBoxOutpurDir.Text;
+            JpegSmallerCS.Properties.Settings.Default.SmallerTo = textBoxSmallerTo.Text;
+            JpegSmallerCS.Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxInputFiles.Text = JpegSmallerCS.Properties.Settings.Default.InputFileName;
+            textBoxOutpurDir.Text = JpegSmallerCS.Properties.Settings.Default.OutputDir;
+            textBoxSmallerTo.Text = JpegSmallerCS.Properties.Settings.Default.SmallerTo;
         }
     }
 }
